7cb93390832b5c9088c0a019cd1a0a0ee5c2aa6e
Johan Sandgren <gussanjoba@student.gu.se>
2023-09-05 18:21:35 +0200

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@ const message = ref('');
 
 onMounted(async () => {
   try {
-    const response = await fetch('http://localhost:3000/api'); // Change the URL as needed
+    const response = await fetch('https://apitest-n6l3.onrender.com/api'); // Change the URL as needed
     if (response.ok) {
       message.value = await response.text();
     } else {
