47dabb14206a0b6a14e7d9a90532382e7c06cd93
Johan Sandgren <gussanjoba@student.gu.se>
2023-09-05 18:09:03 +0200

    s

---
+++ server/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var cors = require('cors');
+
+
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+// Parse requests of content-type 'application/json'
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// HTTP request logger
+app.use(morgan('dev'));
+// Enable cross-origin resource sharing for frontend must be registered before api
+app.options('*', cors());
+// set up cors for port 5173
+app.use(cors({origin: 'http://localhost:5173'}));
+
+// Import routes
+app.get('/api', function(req, res) {
+    res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
+});
+
+
+// Serve static assets
+var root = path.normalize(__dirname + '/..');
+var client = path.join(root, 'client', 'dist');
+app.use(express.static(client));
+
+
+
+
+
+app.listen(port, function(err) {
+    if (err) throw err;
+    console.log('Express server listening on port ' + port);
+});
+
+
+module.exports = app;
---
+++ vite-project/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, onMounted } from 'vue';
+
+const message = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api'); // Change the URL as needed
+    if (response.ok) {
+      message.value = await response.text();
+    } else {
+      console.error('Failed to fetch message:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching message:', error);
+  }
+});
+</script>
+
+<template>
+  <HelloWorld :msg="message" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
